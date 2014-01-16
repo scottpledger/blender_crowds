05b0ee38d067e1ac4c86e694c0aef2d11cb0a8af
Scott R. Pledger <pledger@colorado.edu>
2014-01-16 13:54:15 -0700

    Began working on the python module.

---
+++ __init__.py
@@ -0,0 +1,26 @@
+import bpy
+bl_info = {
+	"name": "Blender Crowds",
+	"author": "spledger",
+	"version": (1, 0),
+	"blender": (2, 69, 0),
+	"location": "Search > Fracture Object & Add -> Fracture Helper Objects",
+	"description": "Brings crowd animations to Blender.",
+	"warning": "This is an early beta! Nothing yet works!",
+#    "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.6/Py/"\
+#        "Scripts/Object/Fracture",
+#    "tracker_url": "https://projects.blender.org/tracker/index.php?"\
+#        "func=detail&aid=21793",
+	"category": "Animation"
+}
+
+
+def register():
+	pass
+
+def unregister():
+	pass
+
+if __name__ == "__main__":
+	register()
+
